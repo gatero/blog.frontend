b9b5d8de3ce297e7abbbe82fe10dfae4c294d4aa
Daniel Ortega <hola@daniel-ortega.mx>
2023-05-19 15:39:41 -0600

    add i18n labels and devicons

---
+++ src/lang/en/experience.ts
@@ -0,0 +1,658 @@
+export default {
+  works: [
+    {
+      title: 'FREELANCE',
+      time: '2009 - Present',
+      excerpt: 'Working by my side I had  the oportunity of work with a lot of technologies, each proyect is different and always is a good time to learn something different',
+      devicons: [
+        {
+          name: 'gitlab',
+          title: 'Gitlab'
+        },
+        {
+          name: 'git',
+          title: 'Git'
+        },
+        {
+          name: 'mysql',
+          title: 'MySQL'
+        },
+        {
+          name: 'tomcat',
+          title: 'Tomcat'
+        },
+        {
+          name: 'java',
+          title: 'Java'
+        },
+        {
+          name: 'php',
+          title: 'PHP'
+        },
+        {
+          name: 'wordpress',
+          title: 'Wordpress'
+        },
+        {
+          name: 'html',
+          title: 'HTML'
+        },
+        {
+          name: 'js',
+          title: 'Javascript'
+        },
+        {
+          name: 'jquery',
+          title: 'jQuery'
+        },
+        {
+          name: 'foundation',
+          title: 'Foundation'
+        },
+        {
+          name: 'bootstrap',
+          title: 'Bootstrap'
+        },
+        {
+          name: 'css',
+          title: 'CSS'
+        },
+        {
+          name: 'sass',
+          title: 'Sass'
+        },
+        {
+          name: 'less',
+          title: 'less'
+        },
+        {
+          name: 'vim',
+          title: 'Vim'
+        },
+        {
+          name: 'linux',
+          title: 'Linux'
+        },
+        {
+          name: 'docker',
+          title: 'Docker'
+        },
+        {
+          name: 'golang',
+          title: 'Go'
+        },
+        {
+          name: 'nodejs',
+          title: 'Nose.js'
+        },
+        {
+          name: 'vuejs',
+          title: 'Vue.js'
+        },
+        {
+          name: 'ts',
+          title: 'Typescript'
+        },
+      ],
+    },
+    {
+      title: 'IDAAPPS',
+      time: '12 Mar 2020 - present',
+      excerpt: 'I started making an auditory to the development team of the company and nowadays I re build their wordpress site and now I\'m working in a new version bases on javascript and golang',
+      devicons: [
+        {
+          name: 'gitlab',
+          title: 'Gitlab'
+        },
+        {
+          name: 'git',
+          title: 'Git'
+        },
+        {
+          name: 'vim',
+          title: 'Vim'
+        },
+        {
+          name: 'linux',
+          title: 'Linux'
+        },
+        {
+          name: 'docker',
+          title: 'Docker'
+        },
+        {
+          name: 'golang',
+          title: 'Go'
+        },
+        {
+          name: 'nodejs',
+          title: 'Nose.js'
+        },
+        {
+          name: 'php',
+          title: 'PHP'
+        },
+        {
+          name: 'wordpress',
+          title: 'Wordpress'
+        },
+        {
+          name: 'vuejs',
+          title: 'Vue.js'
+        },
+        {
+          name: 'ts',
+          title: 'Typescript'
+        },
+        {
+          name: 'sass',
+          title: 'Sass'
+        },
+        {
+          name: 'html',
+          title: 'html'
+        },
+        {
+          name: 'css',
+          title: 'CSS'
+        },
+        {
+          name: 'js',
+          title: 'javascript'
+        },
+      ],
+    },
+    {
+      title: 'GUROS',
+      time: '12 Mar 2020 - 30 Aug 2020',
+      excerpt: 'I help them with the first re build step of their site, passing from Laravel to node.js with AWS Lambdas structuring the multisite support',
+      devicons: [
+        {
+          name: 'gitlab',
+          title: 'Gitlab'
+        },
+        {
+          name: 'git',
+          title: 'Git'
+        },
+        {
+          name: 'vim',
+          title: 'Vim'
+        },
+        {
+          name: 'linux',
+          title: 'Linux'
+        },
+        {
+          name: 'docker',
+          title: 'Docker'
+        },
+        {
+          name: 'aws',
+          title: 'AWS'
+        },
+        {
+          name: 'nodejs',
+          title: 'Nose.js'
+        },
+        {
+          name: 'php',
+          title: 'PHP'
+        },
+        {
+          name: 'laravel',
+          title: 'Laravel'
+        },
+        {
+          name: 'html',
+          title: 'html'
+        },
+        {
+          name: 'css',
+          title: 'CSS'
+        },
+        {
+          name: 'js',
+          title: 'javascript'
+        },
+      ],
+    },
+    {
+      title: 'CULTURA COLECTIVA',
+      time: 'Oct 2017 - Jan 2020',
+      excerpt: 'My work there is create, maintain and find the best ways to solve problems in a short time using technologies such as bash, Docker, Golang, gRPC, mongodb, React, Redux, Vue, typescript, babel, firebase, aws and scss(css3), all those technologies work together in a ecosystem of microservices created in part by me.',
+      devicons: [
+        {
+          name: 'gitlab',
+          title: 'Gitlab'
+        },
+        {
+          name: 'git',
+          title: 'Git'
+        },
+        {
+          name: 'vim',
+          title: 'Vim'
+        },
+        {
+          name: 'linux',
+          title: 'Linux'
+        },
+        {
+          name: 'docker',
+          title: 'Docker'
+        },
+        {
+          name: 'golang',
+          title: 'Go'
+        },
+        {
+          name: 'nodejs',
+          title: 'Nose.js'
+        },
+        {
+          name: 'php',
+          title: 'PHP'
+        },
+        {
+          name: 'wordpress',
+          title: 'Wordpress'
+        },
+        {
+          name: 'html',
+          title: 'HTML'
+        },
+        {
+          name: 'react',
+          title: 'React'
+        },
+        {
+          name: 'redux',
+          title: 'Redux'
+        },
+        {
+          name: 'vuejs',
+          title: 'Vue.js'
+        },
+        {
+          name: 'js',
+          title: 'javascript'
+        },
+        {
+          name: 'ts',
+          title: 'Typescript'
+        },
+        {
+          name: 'css',
+          title: 'CSS'
+        },
+        {
+          name: 'sass',
+          title: 'Sass'
+        },
+      ],
+    },
+    {
+      title: 'GLOBANT',
+      time: 'Apr 2017 - Oct 2017',
+      excerpt: 'I worked with angular 4 and SCSS developing components and consuming microservices from their internal API\'s, I was assigned to the internal collaborator management project',
+      devicons: [
+        {
+          name: 'gitlab',
+          title: 'Gitlab'
+        },
+        {
+          name: 'git',
+          title: 'Git'
+        },
+        {
+          name: 'vim',
+          title: 'Vim'
+        },
+        {
+          name: 'linux',
+          title: 'Linux'
+        },
+        {
+          name: 'docker',
+          title: 'Docker'
+        },
+        {
+          name: 'nodejs',
+          title: 'Nose.js'
+        },
+        {
+          name: 'angular',
+          title: 'angular'
+        },
+        {
+          name: 'js',
+          title: 'javascript'
+        },
+        {
+          name: 'ts',
+          title: 'Typescript'
+        },
+        {
+          name: 'sass',
+          title: 'Sass'
+        },
+      ],
+    },
+    {
+      title: 'VINCO ORBIS',
+      time: 'Apr 2015 - Apr 2017',
+      excerpt: 'I\'ve been working within different areas, from backend to frontend and a little bit of DevOps, I was working with different technologies such as PHP, Wordpress, Javascript, Node.js, Docker, Bootstrap, React.js and Angularjs, part of my responsibilities was procure the implementation of design patterns and good practices through many projects, I started as frontend and after few months I start as Full stack with nodejs',
+      devicons: [
+        {
+          name: 'gitlab',
+          title: 'Gitlab'
+        },
+        {
+          name: 'git',
+          title: 'Git'
+        },
+        {
+          name: 'vim',
+          title: 'Vim'
+        },
+        {
+          name: 'linux',
+          title: 'Linux'
+        },
+        {
+          name: 'mysql',
+          title: 'MySQL'
+        },
+        {
+          name: 'mongo',
+          title: 'Mongo DB'
+        },
+        {
+          name: 'docker',
+          title: 'Docker'
+        },
+        {
+          name: 'nodejs',
+          title: 'Nose.js'
+        },
+        {
+          name: 'php',
+          title: 'PHP'
+        },
+        {
+          name: 'wordpress',
+          title: 'Wordpress'
+        },
+        {
+          name: 'sass',
+          title: 'Sass'
+        },
+        {
+          name: 'less',
+          title: 'less'
+        },
+        {
+          name: 'html',
+          title: 'HTML'
+        },
+        {
+          name: 'js',
+          title: 'Javascript'
+        },
+        {
+          name: 'angular',
+          title: 'Angular'
+        },
+        {
+          name: 'react',
+          title: 'React'
+        },
+        {
+          name: 'jquery',
+          title: 'jQuery'
+        },
+        {
+          name: 'bootstrap',
+          title: 'Bootstrap'
+        },
+        {
+          name: 'css',
+          title: 'CSS'
+        },
+      ],
+    },
+    {
+      title: 'BANCO AZTECA',
+      time: 'Apr 2014 - Mar 2015',
+      excerpt: 'There I was doing maintenance to the old version of their system and I worked in the restructuring of the new look and feel for the user account manager',
+      devicons: [
+        {
+          name: 'java',
+          title: 'Java'
+        },
+        {
+          name: 'html',
+          title: 'HTML'
+        },
+        {
+          name: 'js',
+          title: 'Javascript'
+        },
+        {
+          name: 'jquery',
+          title: 'jQuery'
+        },
+        {
+          name: 'bootstrap',
+          title: 'Bootstrap'
+        },
+        {
+          name: 'css',
+          title: 'CSS'
+        },
+      ],
+    },
+    {
+      title: 'MUSEO DEL PALACIO DE BELLAS ARTES',
+      time: 'Jan 2014 - Sep 2014',
+      excerpt: 'When Ii\'ve been working there I was involved on different parts of the development of a cms for museum users, it was developed using Java for backend and Javascript for frontend my part in this was the architecture of frontend with jQuery and css by the creation of components, another of my responsibilities was the creation of static sites for museum\'s expositions',
+      devicons: [
+        {
+          name: 'gitlab',
+          title: 'Gitlab'
+        },
+        {
+          name: 'git',
+          title: 'Git'
+        },
+        {
+          name: 'mysql',
+          title: 'MySQL'
+        },
+        {
+          name: 'tomcat',
+          title: 'Tomcat'
+        },
+        {
+          name: 'java',
+          title: 'Java'
+        },
+        {
+          name: 'php',
+          title: 'PHP'
+        },
+        {
+          name: 'wordpress',
+          title: 'Wordpress'
+        },
+        {
+          name: 'html',
+          title: 'HTML'
+        },
+        {
+          name: 'js',
+          title: 'Javascript'
+        },
+        {
+          name: 'jquery',
+          title: 'jQuery'
+        },
+        {
+          name: 'foundation',
+          title: 'Foundation'
+        },
+        {
+          name: 'bootstrap',
+          title: 'Bootstrap'
+        },
+        {
+          name: 'css',
+          title: 'CSS'
+        },
+        {
+          name: 'sass',
+          title: 'Sass'
+        },
+        {
+          name: 'less',
+          title: 'less'
+        },
+      ],
+    },
+    {
+      title: 'MUSEO NACIONAL DE ARTE',
+      time: 'Jan 2012 - Dec 2013',
+      excerpt: 'This was the first opportunity that I have to work in a museum, and was great, there I\'ve been working with technologies as Coffescript, jQuery, Css, Angular, PHP, Wordpress and Java, MySQL, my responsibilities was create static sites for museum\'s expositions, maintenance of museum\'s official site and a CMS for internal workers of the museum to be able them organize the artwork easely',
+      devicons: [
+        {
+          name: 'gitlab',
+          title: 'Gitlab'
+        },
+        {
+          name: 'git',
+          title: 'Git'
+        },
+        {
+          name: 'mysql',
+          title: 'MySQL'
+        },
+        {
+          name: 'tomcat',
+          title: 'Tomcat'
+        },
+        {
+          name: 'java',
+          title: 'Java'
+        },
+        {
+          name: 'php',
+          title: 'PHP'
+        },
+        {
+          name: 'wordpress',
+          title: 'Wordpress'
+        },
+        {
+          name: 'html',
+          title: 'HTML'
+        },
+        {
+          name: 'js',
+          title: 'Javascript'
+        },
+        {
+          name: 'jquery',
+          title: 'jQuery'
+        },
+        {
+          name: 'foundation',
+          title: 'Foundation'
+        },
+        {
+          name: 'bootstrap',
+          title: 'Bootstrap'
+        },
+        {
+          name: 'css',
+          title: 'CSS'
+        },
+        {
+          name: 'sass',
+          title: 'Sass'
+        },
+        {
+          name: 'less',
+          title: 'less'
+        },
+      ],
+    },
+    {
+      title: 'CARNERIO',
+      time: 'Ene 2012 - Dic 2014',
+      excerpt: 'Working at Carnerio I learned how to work with responsive technologies, firs we create simple grid systems for our sites, before we start to use technologies as bootstrap and foundation a side with jQuery in a lot of static sites for different companies and government institutions, when I arrive to Carnerio I\'ve been working as freelancer',
+      devicons: [
+        {
+          name: 'gitlab',
+          title: 'Gitlab'
+        },
+        {
+          name: 'git',
+          title: 'Git'
+        },
+        {
+          name: 'mysql',
+          title: 'MySQL'
+        },
+        {
+          name: 'tomcat',
+          title: 'Tomcat'
+        },
+        {
+          name: 'java',
+          title: 'Java'
+        },
+        {
+          name: 'php',
+          title: 'PHP'
+        },
+        {
+          name: 'wordpress',
+          title: 'Wordpress'
+        },
+        {
+          name: 'html',
+          title: 'HTML'
+        },
+        {
+          name: 'js',
+          title: 'Javascript'
+        },
+        {
+          name: 'jquery',
+          title: 'jQuery'
+        },
+        {
+          name: 'foundation',
+          title: 'Foundation'
+        },
+        {
+          name: 'bootstrap',
+          title: 'Bootstrap'
+        },
+        {
+          name: 'css',
+          title: 'CSS'
+        },
+        {
+          name: 'sass',
+          title: 'Sass'
+        },
+        {
+          name: 'less',
+          title: 'less'
+        },
+      ],
+    },
+  ],
+};
---
+++ src/lang/en/services.ts
@@ -0,0 +1,44 @@
+export default {
+  items: [
+    {
+      title: 'Cultura Colectiva',
+      description: 'My work there is create, maintain and find the best ways to solve problems in a short time using technologies such as bash, Docker, Golang, gRPC, mongodb, React, Redux, Vue, typescript, babel, firebase, aws and scss(css3), all those technologies work together in a ecosystem of microservices created in part by me.',
+      date: 'Oct 2017 - Present',
+    },
+    {
+      title: 'Globant',
+      description: 'I worked with angular 4 and SCSS developing components and consuming microservices from their internal API\'s, I was assigned to the internal collaborator management project',
+      date: 'Apr 2017 - Oct 2017',
+    },
+    {
+      title: 'Vinco Orbis',
+      description: 'I\'ve been working within different areas, from backend to frontend and a little bit of DevOps, I was working with different technologies such as PHP, Wordpress, Javascript, Node.js, Docker, Bootstrap, React.js and Angularjs, part of my responsibilities was procure the implementation of design patterns and good practices through many projects, I started as frontend and after few months I start as Full stack with nodejs See less',
+      date: 'Apr 2015 - Apr 2017',
+    },
+    {
+      title: 'Banco Azteca',
+      description: 'There I was doing maintenance to the old version of their system and I worked in the restructuring of the new look and feel for the user account manager',
+      date: 'Aug 2014 - Mar 2015',
+    },
+    {
+      title: 'Museo Del Palacio de Bellas Artes',
+      description: 'When I\'ve been working there I was involved on different parts of the development of a cms for museum users, it was developed using Java for backend and Javascript for frontend my part in this was the architecture of frontend with jQuery and css by the creation of components, another of my responsibilities was the creation of static sites for museum\'s expositions See less',
+      date: 'Jan 2014 - Sep 2014',
+    },
+    {
+      title: 'Museo Nacional de Arte',
+      description: 'This was the first opportunity that I have to work in a museum, and was great, there I\'ve been working with technologies as Coffescript, jQuery, Css, Angular, PHP, Wordpress and Java, MySQL, my responsibilities was create static sites for museum\'s expositions, maintenance of museum\'s official site and a CMS for internal workers of the museum to be able them organize the artwork easely See less',
+      date: 'Jan 2012 - Dec 2013',
+    },
+    {
+      title: 'Carnerio',
+      description: 'Working on Carnerio I learned to work with responsive technologies, firs we create simple grid systems for our sites, before we start to use technologies as bootstrap and foundation a side with jQuery in a lot of static sites for different companies and government institutions, when I arrive to Carnerio I\'ve been working as freelancer',
+      date: 'Jan 2012 - Dec 2014',
+    },
+    {
+      title: 'Freelance',
+      description: 'Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis.',
+      date: '2009 - Present',
+    },
+  ],
+};
