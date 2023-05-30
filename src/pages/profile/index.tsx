172ca1d7738f4c41d93bef6fa776becfd19dfe0e
Daniel Ortega <hola@daniel-ortega.mx>
2023-05-29 23:07:20 -0600

    DOM-34: base for sidebar

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,8 +1,5 @@
 import ProfileUpdateForm from '@/components/formik/profile/profile-update-form';
-import { Inter } from 'next/font/google';
 import { Col, Container, ListGroup, Row } from 'react-bootstrap';
-
-const inter = Inter({ subsets: ['latin'] });
 
 type TProfileProps = {};
 
@@ -13,16 +10,26 @@ export const getStaticProps = () => ({
 const Profile = (props: TProfileProps) => (
   <Container className="profile">
     <Row>
-      <Col md={2}>
-        <ListGroup>
-          <ListGroup.Item>Cras justo odio</ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Morbi leo risus</ListGroup.Item>
-          <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
-          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+      <Col md={3}>
+        <ListGroup variant="flush">
+          <ListGroup.Item action onClick={() => false}>
+            Información
+          </ListGroup.Item>
+          <ListGroup.Item action onClick={() => false}>
+            Metodo de pago
+          </ListGroup.Item>
+          <ListGroup.Item action onClick={() => false}>
+            Subscripción
+          </ListGroup.Item>
+          <ListGroup.Item action onClick={() => false}>
+            Grupos
+          </ListGroup.Item>
+          <ListGroup.Item action onClick={() => false}>
+            Usuarios
+          </ListGroup.Item>
         </ListGroup>
       </Col>
-      <Col md={10}>
+      <Col md={9}>
         <h1>profile</h1>
         <ProfileUpdateForm handleSubmit={() => false} />
       </Col>
